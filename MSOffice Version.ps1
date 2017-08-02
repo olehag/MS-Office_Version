90254b9a5aa351de3b197cd38c9790d0a06899ff
LordHagen <olehag04@nfk.no>
2017-08-02 15:10:27 +0200

    Minor text edit

--- MSOffice Version.ps1
+++ MSOffice Version.ps1
@@ -1,8 +1,9 @@
+#Lord Hagen / olehag04@nfk.no
+
 #Get list of installed items from Uninstall registery. Get only those with displayname "Like" 'Microsoft Office*'. Get object DisplayName, DisplayVersion. Format output -Autosize.
-Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  ? {$_.displayname -like "Microsoft Office*"} | Select-Object DisplayName, DisplayVersion | Format-Table -AutoSize
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Where-Object {$_.displayname -like "Microsoft Office*"} | Select-Object DisplayName, DisplayVersion | Format-Table -AutoSize
 
 #Press a button to exit
 Write-Host ""
 Write-Host "Press any button to exit..." -ForegroundColor Yellow
 $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-
