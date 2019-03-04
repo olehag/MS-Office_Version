9085c3af9b4edc7657b1db14169aadb6877f0ee1
LordHagen <olehag04@nfk.no>
2019-03-04 10:13:42 +0100

    Text edit

--- MSOffice Version.ps1
+++ MSOffice Version.ps1
@@ -1,5 +1,3 @@
-#Lord Hagen / olehag04@nfk.no
-
 #Get list of installed items from Uninstall registery. Get only those with displayname "Like" 'Microsoft Office*'. Get object DisplayName, DisplayVersion. Format output -Autosize.
 Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Where-Object {$_.displayname -like "Microsoft Office*"} | Select-Object DisplayName, DisplayVersion | Format-Table -AutoSize
 
